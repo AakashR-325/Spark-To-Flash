12853804558e29ec991256e725641f97f263e00d
aakash <aakashramesh018@gmail.com>
2023-10-11 03:35:25 +0530

    setup the boiler plate code for executing flash loan

--- contracts/SparkToFlash.sol
+++ contracts/SparkToFlash.sol
@@ -10,5 +10,47 @@ contract SparkToFlash is FlashLoanSimpleReceiverBase {
 
     constructor(
         address _addressProvider
-    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {}
+    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
+        owner = payable(msg.sender);
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner , "Only contract owner can call this function !");
+        _;
+    }
+
+    function executeOperation(
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        //Add the arbitrage logic here.
+
+        uint256 amountBackToPool = amount + premium;
+        IERC20(assset).approove(address(POOL) , amountBackToPool);
+
+        return true;
+    };
+
+    function flashLoanRequest(address _token , uint _amount) public {
+        POOL.flashLoanSimple(
+            address(this), //receiverAddress
+            _token , //asset
+            _amount , //amount
+            "", //params (placeholder value)
+            0 // refferalCode(placeholder value)
+        );
+
+    }
+
+    function getBalance(address _tokenAddress) external view returns{
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress) external onlyOwner{
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender , token.balanceOf(address(this)));
+    }
 }
